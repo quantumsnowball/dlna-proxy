67a309265e9de553e5f09f54a9585dcfe0ce4d64
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-01 19:58:47 +0800

    EXTRA \r\n at the end is a MUST !!!!!
    without this \r\n at the end VLC won't be able to parse that request, I
    don't know why this is important, but only if I added this back this
    just work! VLC will be able to see it immediately!!!

--- ssdp/server.py
+++ ssdp/server.py
@@ -29,7 +29,7 @@ class Server:
                 f'USN: uuid:{self.uuid}::{self.media_type}',
                 'CACHE-CONTROL: max-age=1800',
                 f'LOCATION: {self.location}',
-                '',
+                '\r\n',
             )).encode("utf-8")
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
@@ -61,7 +61,7 @@ class Server:
                 f'St: {self.media_type}',
                 f'Usn: uuid:{self.uuid}::{self.media_type}',
                 'Content-Length: 0',
-                '',
+                '\r\n',
             )).encode('utf-8')
         try:
             sock.bind(('0.0.0.0', self.ssdp_port))
